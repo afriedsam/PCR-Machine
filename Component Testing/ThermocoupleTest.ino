8a7f6be32b1011fd67bff816e0470bb97388bcf7
Aidan Friedsam <adf@thefriedsams.com>
2020-01-22 11:05:53 -0500

    Create ThermocoupleTest.ino

---
+++ Component Testing/ThermocoupleTest.ino
@@ -0,0 +1,24 @@
+#include <Adafruit_MAX31855.h>
+#define MAXDO   3
+#define MAXCS   4
+#define MAXCLK  5
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
+double temp = thermocouple.readCelsius();
+
+/* Pin Layout:
+ *  Thermocouple
+ *    DO to D3 on arduino
+ *    CS to D4 on arduino
+ *    CLK to D5 on arduino
+ *    VCC to 5V on arduino
+ *    GND to GND on arduino
+*/
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(temp);
+  delay(500);
+}
